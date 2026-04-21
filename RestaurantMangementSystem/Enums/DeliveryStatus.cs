217a97a9be4028a88f4cf8032d8064e8604057b2
jasmine <jasminemohamed174005@gmail.com>
2026-04-21 10:51:12 +0200

    Adding The Enums

---
+++ RestaurantMangementSystem/Enums/DeliveryStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMangementSystem.Enums
+{
+    internal enum DeliveryStatus
+    {
+        AwaitingAssignment,
+        OnTheWay,
+        Deliverd,
+        Failed
+    }
+}
